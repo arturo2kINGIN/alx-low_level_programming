70e2a9bbe154fe04506922bc6c48bded1a1dfbf5
arturo2kINGIN <colloarthur85@gmail.com>
2022-09-09 12:10:26 -0700

    my fourth commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main entry point 
- *
- *Return: Always 0 (Success)
+* main - entry point 
+*
+*Return: Always 0 (Success)
 */
- int main (void)
+ int main(void)
  { puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+return (0);
  }
 
