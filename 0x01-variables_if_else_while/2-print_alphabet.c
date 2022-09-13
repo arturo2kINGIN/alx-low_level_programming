8ce934b68388f3b16c8321ed8b39692675a22bb1
arturo2kINGIN <colloarthur85@gmail.com>
2022-09-13 16:57:38 +0000

    my boss commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,16 +5,14 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 
 {
 	char ch;
-	
+
 
 	for (ch = 'a'; ch <= 'z'; ch++)
-	    
-	    putchar(ch);
+		putchar(ch);
 
 	    putchar('\n');
 
