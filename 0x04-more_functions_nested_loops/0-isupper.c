a43e4227ce019debaed12c90e48a446c715771ce
arturo2kINGIN <colloarthur85@gmail.com>
2022-09-16 18:40:23 +0000

    my first commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,37 +1,22 @@
-// C program to demonstrate
-// isupper() function
-
-#include <ctype.h>
+#include "main.h"
 #include <stdio.h>
-
-// called function
-int ttl_upper(int i, int counter)
-{
-	char ch;
-	char a[50] = "GeeksForGeeks";
-	ch = a[0];
-
-	// counting of upper case
-	while (ch != '\0') {
-		ch = a[i];
-		if (isupper(ch))
-			counter++;
-
-		i++;
-	}
-
-	// returning total number of upper case present in sentence
-	return (counter);
-}
 int main()
 {
-	int i = 0;
-	int counter = 0;
+   char c;
+   
+   printf ("Enter a character n");
+   scanf ("%c", &c);
 
-	// calling function
-	counter = ttl_upper(i, counter);
-	printf("\nNumber of upper case present in the sentence is : %d", counter);
-	return 0;
+   if (c>64 && c<91)
+   {
+         printf ("It is uppercase character");   
+   }     
+    else
+   {
+         printf ("It is in lowercase character");
+   }
+
+   return 0;
 }
 
 
