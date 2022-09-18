ccea0feb9ba9f17b6b6df0a1955817be010a5d4f
arturo2kINGIN <colloarthur85@gmail.com>
2022-09-18 21:07:58 +0000

    my seventh commit

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,12 +1,10 @@
 #include "main.h"
 
-
 /**
  * print_triangle - Function that prints a triangle, followed by a new line.
  * @size: the int for the paramaters of my function
  * Return: Always 0.
  */
-
 void printf_triangle(int size)
 
 {
