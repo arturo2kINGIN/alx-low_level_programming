af05634f94b140959454e81c6d92dd593415bd39
arturo2kINGIN <colloarthur85@gmail.com>
2022-10-23 15:42:35 +0000

    my boss1 commit

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,10 @@
+#include <stdio.h>
+
+#ifndef FUNCTION_POINTERS_
+#define FUNCTION_POINTERS_
+
 void print_name(char *name, void (*f)(char *));
-int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
+int _putchar( char c);
+#endif /* #ifndef FUNCTION_POINTERS_ */
